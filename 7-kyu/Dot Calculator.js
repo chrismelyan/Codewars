5d89f4773679af50a1855813843d995249a55006
chrismelyan <chris.shelpuk@gmail.com>
2022-03-30 23:28:43 +0200

    Dot Calculator (7)

---
+++ 7-kyu/Dot Calculator.js
@@ -0,0 +1,51 @@
+// Dot Calculator
+
+// You have to write a calculator that receives strings for input.
+// The dots will represent the number in the equation.
+// There will be dots on one side, an operator, and dots again after the operator.
+// The dots and the operator will be separated by one space.
+
+// Here are the following valid operators :
+//
+//     + Addition
+//     - Subtraction
+//     * Multiplication
+//     // Integer Division
+//
+// Examples (Input => Output)
+// * "..... + ..............." => "...................."
+// * "..... - ..."             => ".."
+// * "..... - ."               => "...."
+// * "..... * ..."             => "..............."
+// * "..... * .."              => ".........."
+// * "..... // .."             => ".."
+// * "..... // ."              => "....."
+// * ". // .."                 => ""
+// * ".. - .."                 => ""
+
+function dotCalculator (equation) {
+    let num = 0;
+    let arr = equation.replace(/\s/g, '').split('');
+    const findOperator = (operator) => {
+        return operator === '+'|| operator === '-'|| operator === '*'|| operator === '/';
+    }
+    let operator = arr.findIndex(findOperator);
+
+    const leftPart = arr.slice(0, operator);
+    const rightPart = arr.slice(operator +1);
+
+    if (arr[operator] === '+') {
+        num = leftPart.length+rightPart.length;
+    } else if (arr[operator] === '-') {
+        num = leftPart.length-rightPart.length;
+    } else if (arr[operator] === '*') {
+        num = leftPart.length*rightPart.length;
+    } else if (arr[operator] === '/') {
+        if(leftPart.length < rightPart.length-1) {
+            num = 0;
+        } else {
+            num = leftPart.length/(rightPart.length-1);
+        }
+    }
+    return num === 0 ? '' : '.'.repeat(num);
+}
